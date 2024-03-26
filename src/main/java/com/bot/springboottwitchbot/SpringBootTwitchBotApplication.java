c356d44a1de0459837043ff31600e179f30c9342
MaxonRash <rashnikov.m@yandex.com>
2024-03-26 20:17:24 +0300

    Empty Spring Boot Project

---
+++ src/main/java/com/bot/springboottwitchbot/SpringBootTwitchBotApplication.java
@@ -0,0 +1,13 @@
+package com.bot.springboottwitchbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTwitchBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTwitchBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bot/springboottwitchbot/SpringBootTwitchBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bot.springboottwitchbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTwitchBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
