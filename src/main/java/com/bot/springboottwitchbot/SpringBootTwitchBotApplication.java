01fa285d1804da596159258517714b076015e525
MaxonRash <rashnikov.m@yandex.com>
2024-03-26 22:33:15 +0300

    .gitignore update DataBase config

--- src/main/java/com/bot/springboottwitchbot/SpringBootTwitchBotApplication.java
+++ src/main/java/com/bot/springboottwitchbot/SpringBootTwitchBotApplication.java
@@ -1,13 +1,41 @@
 package com.bot.springboottwitchbot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
+@PropertySource("classpath:databaseCredentials.properties")
 public class SpringBootTwitchBotApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootTwitchBotApplication.class, args);
     }
+
+    //DataSource Config (postgres)
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
+    }
+
 
 }
