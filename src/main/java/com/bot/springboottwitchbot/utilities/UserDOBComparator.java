e53b7888c934dc7c42232d56d94613923e03ab9f
MaxonRash <rashnikov.m@yandex.com>
2024-05-07 15:27:20 +0300

    null check fix

--- src/main/java/com/bot/springboottwitchbot/utilities/UserDOBComparator.java
+++ src/main/java/com/bot/springboottwitchbot/utilities/UserDOBComparator.java
@@ -7,6 +7,10 @@ import java.util.Comparator;
 public class UserDOBComparator implements Comparator<User> {
     @Override
     public int compare(User o1, User o2) {
+        if (o1.getDateOfBirth() == null)
+            return 100;
+        if (o2.getDateOfBirth() == null)
+            return -100;
         return o1.getDateOfBirth().getMonth() - o2.getDateOfBirth().getMonth();
     }
 }
